8d67550bab74e6b9ad84eb121fbf1cab39429e59
hienpvptit <36995026+hienpvptit@users.noreply.github.com>
2018-09-27 17:47:30 +0700

    Add files via upload

---
+++ Ex4 - DHT11 Sensor/1_Read_DHT11_Sensor/1_Read_DHT11_Sensor.ino
@@ -0,0 +1,21 @@
+/*
+  Read DHT11 Sensor
+  
+  Read Value Light from Light Sensor
+
+  Schematic:
+  DHT11 Sensor <---> D4
+
+  This example code is in the public domain.
+  http://www.lophocvui.com/
+*/
+#include "DHT.h"
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
