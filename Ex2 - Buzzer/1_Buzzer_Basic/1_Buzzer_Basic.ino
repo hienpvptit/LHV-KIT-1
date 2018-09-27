3c7101878c41fb77461b0c55b250dadef9b98084
hienpvptit <36995026+hienpvptit@users.noreply.github.com>
2018-09-27 17:13:06 +0700

    Add files via upload

---
+++ Ex2 - Buzzer/1_Buzzer_Basic/1_Buzzer_Basic.ino
@@ -0,0 +1,54 @@
+/*
+  Buzzer Basic
+  
+  Turn ON/OFF Buzzer
+
+  Schematic:
+  Cathode_Buzzer(-) <---> 0 V
+  Anode_Buzzer  (+) <---> D8
+
+  =>
+  D = 1 (true, 3.3V) : Buzzer ON
+  D = 0 (false, 0V)  : Buzzer OFF 
+
+  This example code is in the public domain.
+  http://www.lophocvui.com/
+*/
+#define BUZZER D8
+#define ON  1
+#define OFF 0
+
+void setup() {
+  // Setup BUZZER to OUTPUT PIN
+  pinMode(BUZZER, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  // Buzzer basic 1
+  for(int i=0; i<5; i++)
+  {
+    digitalWrite(BUZZER, ON);
+    delay(1000);
+    digitalWrite(BUZZER, OFF);
+    delay(1000);
+  }
+
+  // Buzzer basic 2
+  for(int i=0; i<3; i++)
+  {
+    digitalWrite(BUZZER, ON);
+    delay(200);
+    digitalWrite(BUZZER, OFF);
+    delay(50);
+  }
+  digitalWrite(BUZZER, ON);
+  delay(600);
+  digitalWrite(BUZZER, OFF);
+  delay(50);
+  digitalWrite(BUZZER, ON);
+  delay(200);
+  digitalWrite(BUZZER, OFF);
+  delay(50);
+}
---
+++ Ex3 - Light Sensor/1_Read_Light_Sensor/1_Read_Light_Sensor.ino
@@ -0,0 +1,26 @@
+/*
+  Read Light Sensor
+  
+  Read Value Light from Light Sensor
+
+  Schematic:
+  Light Sensor <---> A0
+
+  =>
+  readAnalog(A0); 
+
+  This example code is in the public domain.
+  http://www.lophocvui.com/
+*/
+void setup() {
+  // Setup Serial Baud rate
+  Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  float light = analogRead(A0); // Value From 0 to 1023
+  Serial.print("Light: "); 
+  Serial.println(light);
+  delay(100);
+}
