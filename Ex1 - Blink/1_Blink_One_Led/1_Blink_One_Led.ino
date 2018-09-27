1238db2699080ceabe1a539d9c97f70040e18e5c
hienpvptit <36995026+hienpvptit@users.noreply.github.com>
2018-09-27 17:12:40 +0700

    Add files via upload

---
+++ Ex1 - Blink/1_Blink_One_Led/1_Blink_One_Led.ino
@@ -0,0 +1,34 @@
+/*
+  Blink One Led
+  
+  Turns an LED on for one second, then off for one second, repeatedly.
+
+  Schematic:
+  Cathode_Led (-) <---> 0 V
+  Anode_Led   (+) <---> D0
+
+  =>
+  D0 = 1 (true, 3.3V) : Led ON
+  D0 = 0 (false, 0V)  : Led OFF 
+
+  This example code is in the public domain.
+  http://www.lophocvui.com/
+*/
+
+#define LED D0  // Define LED = D0
+#define OFF 0   // Define OFF = 0
+#define ON  1   // Define ON  = 1
+
+void setup() {
+  // Setup LED to OUTPUT PIN
+  pinMode(LED, OUTPUT);
+
+}
+
+void loop() {
+  // Program at here will run repeatedly:
+  digitalWrite(LED, ON);  // Set Led ON
+  delay(1000);            // Wait for 1 sec
+  digitalWrite(LED, OFF); // Set Led OFF
+  delay(1000);            // Wait for 1 sec
+}
---
+++ Ex1 - Blink/2_Blink_More_Led/2_Blink_More_Led.ino
@@ -0,0 +1,65 @@
+/*
+  Blink More Leds
+  
+  Create some effects for three LED on Board
+
+  Schematic:
+  Cathode_Leds(-) <---> 0 V
+  Anode_Led_1 (+) <---> D0
+  Anode_Led_2 (+) <---> D5
+  Anode_Led_3 (+) <---> D6
+
+  =>
+  D = 1 (true, 3.3V) : Led ON
+  D = 0 (false, 0V)  : Led OFF 
+
+  This example code is in the public domain.
+  http://www.lophocvui.com/
+*/
+
+#define LED_1 D0  // Define LED_1 = D0
+#define LED_2 D5  // Define LED_2 = D5
+#define LED_3 D6  // Define LED_3 = D6
+#define ON  1     // Define ON    = 1
+#define OFF 0     // Define OFF   = 0
+
+void setup() {
+  // Setup LEDs to OUTPUT PIN
+  pinMode(LED_1, OUTPUT);
+  pinMode(LED_2, OUTPUT);
+  pinMode(LED_3, OUTPUT);
+}
+
+void loop() {
+  // Program at here will run repeatedly:
+
+  // Blink Led
+  for(int i=0; i<5; i++)
+  {
+    digitalWrite(LED_1, ON);    // Set Led_1 ON
+    digitalWrite(LED_2, ON);    // Set Led_2 ON
+    digitalWrite(LED_3, ON);    // Set Led_3 ON
+    delay(1000);                // Wait for 1 sec
+    digitalWrite(LED_1, OFF);   // Set Led_1 OFF
+    digitalWrite(LED_2, OFF);   // Set Led_1 OFF
+    digitalWrite(LED_3, OFF);   // Set Led_1 OFF
+    delay(1000);                // Wait for 1 sec
+  }
+  
+  // Light Queue ON
+  digitalWrite(LED_1, ON);
+  delay(1000);
+  digitalWrite(LED_2, ON);
+  delay(1000);
+  digitalWrite(LED_3, ON);
+  delay(1000);
+
+  // Light Queue OFF
+  digitalWrite(LED_1, OFF);
+  delay(1000);
+  digitalWrite(LED_2, OFF);
+  delay(1000);
+  digitalWrite(LED_3, OFF);
+  delay(1000);
+ 
+}
